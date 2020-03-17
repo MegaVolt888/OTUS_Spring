ece2c2cddfdcc4b63dc54951accefa3792e6c135
k.sorokin <k.sorokin@ntc-vulkan.ru>
2020-03-17 17:04:31 +0300

    begin

---
+++ HomeWork03/src/main/java/ru/sorokinkv/homework03/HomeWork03Application.java
@@ -0,0 +1,13 @@
+package ru.sorokinkv.homework03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork03Application.class, args);
+    }
+
+}
---
+++ HomeWork03/src/test/java/ru/sorokinkv/homework03/HomeWork03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokinkv.homework03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
