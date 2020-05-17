4e6e92639b7c847be983d60816ecc1613fc5a34a
MegaVolt888 <1_megavolt@mail.ru>
2020-05-17 18:23:27 +0300

    hw3

---
+++ HomeWork05_L07/src/test/java/ru/sorokinkv/HomeWork05_L07/HomeWork05L07ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokinkv.HomeWork05_L07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork05L07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
