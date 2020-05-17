7c6c640e4b5c629883cf98b851be491b04bb7d96
MegaVolt888 <1_megavolt@mail.ru>
2020-05-17 10:55:09 +0300

    hw4_only

--- HomeWork03_L04/src/main/java/ru/sorokinkv/homework03/service/QuestionsService.java
+++
@@ -1,9 +0,0 @@
-package ru.sorokinkv.homework03.service;
-
-import ru.sorokinkv.homework03.model.Question;
-
-import java.util.ArrayList;
-
-public interface QuestionsService {
-    ArrayList<Question> getAllQuestion();
-}
--- HomeWork03_L04/src/main/java/ru/sorokinkv/homework03/service/TestStudent.java
+++
@@ -1,5 +0,0 @@
-package ru.sorokinkv.homework03.service;
-
-public interface TestStudent {
-    void testStudent();
-}
